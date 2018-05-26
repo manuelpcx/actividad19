34194a5a30439826ffa6e44b90b41462137dd363
Jesus Medina Aguilera <jesus.medina05@gmail.com>
2018-05-25 20:51:53 -0400

    modificacion del archivo cars

--- cars.rb
+++ cars.rb
@@ -1,4 +1,24 @@
 require_relative 'vehicle'
 class Car < Vehicle
+  @@cont_instancias = 0
+  def initialize
+    @@cont_instancias += 1
+  end
 
+  def self.cant_instancia
+    @@cont_instancias
+  end
+
+  def engine_start
+    @start = true
+    puts 'El motor se ha encendido'
+  end
 end
+
+1..10.times do
+  Car.new
+end
+
+puts Car.new.engine_start
+
+puts Car.cant_instancia
